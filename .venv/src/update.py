3c8fab868154acd342c07b33c5ebd2c790253fa6
pykalka <potapov.kirill.a@gmail.com>
2026-03-26 14:55:13 +0300

    Remove unnecessary files (.venv/lib/, .venv/Scripts/, .venv/share/ .idea, pyvenv.cfg)

--- .venv/src/update.py
+++
@@ -1,8 +0,0 @@
-import joblib
-from src.utils import MODEL_PATH
-
-
-def online_update(text, true_label, model, vectorizer):
-    X_new = vectorizer.transform([text])
-    model.partial_fit(X_new, [true_label])
-    joblib.dump(model, MODEL_PATH)
--- .venv/src/utils.py
+++
@@ -1,3 +0,0 @@
-MODEL_PATH = "model/model.joblib"
-VECTORIZER_PATH = "model/vectorizer.joblib"
-DATA_PATH = "data/spam1.csv"
